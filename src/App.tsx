1745329b950a25806a40e70509d398b313337b9d
이호연 <ho991217@kakao.com>
2022-06-20 12:09:56 +0900

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Spline, { SplineEvent } from "@splinetool/react-spline";
+import React, { useEffect } from "react";
 
-function App() {
+export default function App() {
+  const [equation, setEquation] = React.useState("");
+  const inputRef = React.useRef<HTMLInputElement>(null);
+  function onMouseDown(e: SplineEvent): void {
+    console.log(e.target.name);
+    if (e.target.name === "Key1") {
+      setEquation((prev) => prev + "1");
+    } else if (e.target.name === "Key2") {
+      setEquation((prev) => prev + "2");
+    } else if (
+      e.target.name === "Key+" &&
+      equation[equation.length - 1] !== "+"
+    ) {
+      setEquation((prev) => prev + "+");
+    } else if (
+      e.target.name === "Key=" &&
+      equation[equation.length - 1] !== "+" &&
+      equation.length > 1
+    ) {
+      // eslint-disable-next-line no-eval
+      alert(equation + "=" + eval(equation));
+      setEquation("");
+    }
+  }
+
+  function onKeyDown(e: React.KeyboardEvent<HTMLDivElement>): void {
+    const key = e.key;
+    if (key === "1") {
+      setEquation((prev) => prev + "1");
+    } else if (key === "2") {
+      setEquation((prev) => prev + "2");
+    } else if (key === "+" && equation[equation.length - 1] !== "+") {
+      setEquation((prev) => prev + "+");
+    } else if (
+      key === "=" &&
+      equation[equation.length - 1] !== "+" &&
+      equation.length > 1
+    ) {
+      // eslint-disable-next-line no-eval
+      alert(equation + "=" + eval(equation));
+      setEquation("");
+    }
+  }
+
+  useEffect(() => {
+    if (inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <input
+        style={{ display: "none" }}
+        ref={inputRef}
+        onKeyDown={onKeyDown}
+        onBlur={() => inputRef.current?.focus()}
+      />
+      <Spline
+        scene="https://prod.spline.design/udsQsQSaD-BQCTLH/scene.splinecode"
+        onMouseDown={onMouseDown}
+      />
+    </>
   );
 }
-
-export default App;
